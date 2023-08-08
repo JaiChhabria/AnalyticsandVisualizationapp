809f06ea7176459a295c9a7ab4629a4de83c8d16
JaiChhabria <31588714+JaiChhabria@users.noreply.github.com>
2023-08-08 16:46:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,153 @@
+import streamlit as st
+import pandas as pd
+from textblob import TextBlob
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from PIL import Image
+import openai
+import os
+from dotenv import load_dotenv
+from summarizer import Summarizer
+import docx2txt
+from transformers import AlbertModel
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Set up OpenAI API
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Read data from file
+def read_data(file, file_type):
+    if file_type == "xlsx":
+        df = pd.read_excel(file)
+    elif file_type == "csv":
+        df = pd.read_csv(file)
+    return df
+
+# Generate automatic summary using BERT Extractive Summarizer
+def generate_summary(text, word_limit=100):
+    model = Summarizer()
+    summary = model(text, num_sentences=word_limit // 10)  # Approximate number of sentences based on the word limit
+    return summary
+
+# Redact personal information from text
+def redact_personal_info(text):
+    # Replace personal info with [REDACTED]
+    redacted_text = text.replace("John Doe", "[REDACTED]")
+    redacted_text = redacted_text.replace("johndoe@example.com", "[REDACTED]")
+    # Add more redactions as needed
+    return redacted_text
+
+# Create a word cloud from text
+def create_word_cloud(text):
+    wordcloud = WordCloud(width=800, height=800, background_color='white').generate(text)
+    plt.figure(figsize=(8, 8), facecolor=None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+    return plt
+
+# Create a bar chart
+def create_bar_chart(dataframe, x_column, y_column):
+    plt.figure(figsize=(10, 6))
+    plt.bar(dataframe[x_column], dataframe[y_column])
+    plt.xlabel(x_column)
+    plt.ylabel(y_column)
+    plt.title(f"Bar Chart: {y_column} vs {x_column}")
+    plt.xticks(rotation=45)
+    st.pyplot(plt)
+
+# Create a scatter plot
+def create_scatter_plot(dataframe, x_column, y_column):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(dataframe[x_column], dataframe[y_column])
+    plt.xlabel(x_column)
+    plt.ylabel(y_column)
+    plt.title(f"Scatter Plot: {y_column} vs {x_column}")
+    plt.xticks(rotation=45)
+    st.pyplot(plt)
+
+# Create a chat interaction with the model
+def ask_question(question, df=None, doc_text=None):
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant that provides information based on the uploaded data."},
+        {"role": "user", "content": f"Question: {question}"}
+    ]
+    if df is not None:
+        messages.append({"role": "assistant", "content": f"Data: {df.to_string()}"})
+    if doc_text is not None:
+        messages.append({"role": "user", "content": f"Text: {doc_text}"})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    answer = response.choices[0].message['content']
+    return answer
+
+# Define the main function
+def main():
+    st.title("Data Analysis and Visualization App")
+    st.sidebar.title("Options")
+    
+    uploaded_file = st.sidebar.file_uploader("Upload a file", type=["xlsx", "csv", "docx"])
+    
+    if uploaded_file is not None:
+        file_type = uploaded_file.name.split(".")[-1]
+        
+        if file_type in ["xlsx", "csv"]:
+            df = read_data(uploaded_file, file_type)
+            
+            st.subheader("Data Summary")
+            automatic_summary = generate_summary(df.to_string())
+            st.write(automatic_summary)
+            
+            st.subheader("Ask a Question")
+            user_question = st.text_input("Ask a question about the data")
+            
+            if user_question:
+                answer = ask_question(user_question, df)
+                st.write("Answer:", answer)
+            
+            st.subheader("Chart Creation")
+            chart_type = st.sidebar.selectbox("Select chart type", ["Bar", "Scatter"])
+            
+            if chart_type == "Bar":
+                st.subheader("Bar Chart")
+                x_column = st.sidebar.selectbox("Select X-axis column", df.columns)
+                y_column = st.sidebar.selectbox("Select Y-axis column", df.columns)
+                create_bar_chart(df, x_column, y_column)
+
+            elif chart_type == "Scatter":
+                st.subheader("Scatter Plot")
+                x_column = st.sidebar.selectbox("Select X-axis column", df.columns)
+                y_column = st.sidebar.selectbox("Select Y-axis column", df.columns)
+                create_scatter_plot(df, x_column, y_column)
+            
+            # Include the code for other chart types (Line, Pie, Heat Map) here
+        
+        elif file_type == "docx":
+            # Extract text from uploaded Word document
+            doc_text = docx2txt.process(uploaded_file)
+            
+            st.subheader("Summarized and Redacted Text")
+            
+            # Get user's preferred summary length
+            summary_length = st.number_input("Enter the number of words for summary:", value=100)
+            
+            # Summarize the extracted text with the specified length
+            summarized_text = generate_summary(doc_text, word_limit=summary_length)
+            
+            # Redact personal information from the summarized text
+            redacted_text = redact_personal_info(summarized_text)
+            
+            # Display the summarized and redacted text
+            st.write(redacted_text)
+            
+            st.subheader("Word Cloud")
+            word_cloud_plot = create_word_cloud(redacted_text)
+            st.pyplot(word_cloud_plot)
+
+# Run the app
+if __name__ == "__main__":
+    main()
